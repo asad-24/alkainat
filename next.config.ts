afe3dc2c31051c7a9bb5ded3365341f3e210c857
devasad24 <devasad24@gmail.com>
2025-10-08 12:24:17 +0500

    nwe

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+}
+
+module.exports = nextConfig;
